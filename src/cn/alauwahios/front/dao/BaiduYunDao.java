6e66337652552d5ed7fd09c08457a45a0104da1b
Mr.lu <sekift@163.com>
2018-07-10 17:06:27 +0800

    小调整
    小调整

--- src/cn/alauwahios/front/dao/BaiduYunDao.java
+++ src/cn/alauwahios/front/dao/BaiduYunDao.java
@@ -65,6 +65,7 @@ public class BaiduYunDao {
 
 	/**
 	 * 人工插入的数据
+	 *  默认置顶
 	 * @param vo
 	 * @return
 	 */
@@ -75,7 +76,7 @@ public class BaiduYunDao {
 		}
 		String sql = "INSERT INTO baidu_yun(panShortLink,panLink,"
 				+ " shortLink,createTime,updateTime,type,status,star,sort,hot,visits,remark)"
-				+ " VALUES(?,?,?,now(),now(),?,1,0,1,1,0,?) ON DUPLICATE KEY UPDATE type=1,updateTime=now(),hot=hot+1";
+				+ " VALUES(?,?,?,now(),now(),?,1,0,2,1,0,?) ON DUPLICATE KEY UPDATE type=1,updateTime=now(),hot=hot+1";
 		List<Object> params = new ArrayList<Object>();
 		params.add(vo.getPanShortLink());
 		params.add(vo.getPanLink());

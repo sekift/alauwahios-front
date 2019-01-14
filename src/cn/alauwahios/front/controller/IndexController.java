1cf302ce95ec24aefea71e4c7539b7bd4d4bd74f
Mr.lu <sekift@163.com>
2019-01-14 10:02:48 +0800

    微改
    修改首页

--- src/cn/alauwahios/front/controller/IndexController.java
+++ src/cn/alauwahios/front/controller/IndexController.java
@@ -12,17 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import cn.alauwahios.front.service.BaiduTiebaService;
-import cn.alauwahios.front.service.BaiduTieziService;
 import cn.alauwahios.front.service.BaiduWangpanService;
 import cn.alauwahios.front.service.BaiduYunService;
 import cn.alauwahios.front.service.FxZiyuanService;
-import cn.alauwahios.front.service.InternetCelebrityService;
 import cn.alauwahios.front.vo.BaiduTiebaVO;
-import cn.alauwahios.front.vo.BaiduTieziVO;
 import cn.alauwahios.front.vo.BaiduWangpanVO;
 import cn.alauwahios.front.vo.BaiduYunVO;
 import cn.alauwahios.front.vo.FxZiyuanVO;
-import cn.alauwahios.front.vo.InternetCelebrityVO;
 import cn.alauwahios.front.vo.PageInfo;
 
 @Controller("indexController")
@@ -31,8 +27,8 @@ public class IndexController {
 	@Autowired
 	private BaiduTiebaService baiduTiebaService;
 
-	@Autowired
-	private BaiduTieziService baiduTieziService;
+	//@Autowired
+	//private BaiduTieziService baiduTieziService;
 
 	@Autowired
 	private BaiduWangpanService baiduWangpanService;
@@ -40,8 +36,8 @@ public class IndexController {
 	@Autowired
 	private BaiduYunService baiduYunService;
 	
-	@Autowired
-	private InternetCelebrityService internetCelebrityService;
+	//@Autowired
+	//private InternetCelebrityService internetCelebrityService;
 
 	@Autowired
 	private FxZiyuanService fxZiyuanService;
@@ -60,18 +56,18 @@ public class IndexController {
 			Model model) {
 		List<BaiduTiebaVO> tieba = baiduTiebaService.listBaiduTieba(pageInfo);
 		model.addAttribute("tieba", tieba);
-		List<BaiduTieziVO> tiezi = baiduTieziService.listBaiduTiezi(pageInfo);
-		model.addAttribute("tiezi", tiezi);
 		List<BaiduWangpanVO> wangpan = baiduWangpanService.listBaiduWangpan(pageInfo);
 		model.addAttribute("wangpan", wangpan);
 		List<BaiduYunVO> yun = baiduYunService.listBaiduYun(pageInfo);
 		model.addAttribute("yun", yun);
 		List<BaiduYunVO> make= baiduYunService.listBaiduYunMake(pageInfo);
 		model.addAttribute("make", make);
-		List<InternetCelebrityVO> star = internetCelebrityService.listInternetCelebrity(pageInfo);
-		model.addAttribute("star", star);
 		List<FxZiyuanVO> ziyuan = fxZiyuanService.listFxZiyuan("", pageInfo);
 		model.addAttribute("ziyuan", ziyuan);
+		//List<BaiduTieziVO> tiezi = baiduTieziService.listBaiduTiezi(pageInfo);
+		//model.addAttribute("tiezi", tiezi);
+		//List<InternetCelebrityVO> star = internetCelebrityService.listInternetCelebrity(pageInfo);
+		//model.addAttribute("star", star);
 
 		return "index";
 	}
